e819c5ba8b99ec75d4cfd8bab63aaff7ce9a8296
Abhiraj Trivedi <abhirajtrivedi599@gmail.com>
2026-07-19 18:15:17 +0530

    Updated server for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -88,8 +88,8 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "index.html"));
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
