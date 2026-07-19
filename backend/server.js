a1ebc96b51d7c844f50db0c4c8a940b2b0006e7f
Abhiraj Trivedi <abhirajtrivedi599@gmail.com>
2026-07-19 22:35:44 +0530

    Added SQLite logs

--- backend/server.js
+++ backend/server.js
@@ -105,3 +105,12 @@ app.get("/users", (req, res) => {
         res.json(rows);
     });
 });
+app.get("/users", (req, res) => {
+    db.all("SELECT * FROM users", [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        res.json(rows);
+    });
+});
